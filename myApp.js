85613229d20587e4377104f805138c9243e5c235
Jesús Arribi <arribi@boxer.local>
2024-01-22 12:28:15 +0000

    Create a Model - CRUD Part I: CREATE

--- myApp.js
+++ myApp.js
@@ -3,10 +3,18 @@ mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+});
+
+
 // Person Model
-
-
-let Person;
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
