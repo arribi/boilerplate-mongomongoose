ff17333545cacb3c8636d41b26e9b308435ac1a3
Jesús Arribi <arribi@boxer.local>
2024-01-22 12:43:33 +0000

    Create and Save a Record of a Model

--- myApp.js
+++ myApp.js
@@ -17,7 +17,15 @@ const personSchema = new mongoose.Schema({
 let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  let aPerson = new Person({
+    name: "John",
+    age: 25,
+    favoriteFoods: ["pizza", "pasta", "sushi"]
+  });
+  aPerson.save(function (err, data) {
+    if (err) return console.error(err);
+    done(null, data);
+  })
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
