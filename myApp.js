fb7261522507bfe454191f13a4dade0a900ffe85
Jesús Arribi <arribi@boxer.local>
2024-02-05 09:49:28 +0000

    RemoveById fixed

--- myApp.js
+++ myApp.js
@@ -99,7 +99,7 @@ const findAndUpdate = (personName, done) => {
 const removeById = (personId, done) => {
   Person.findByIdAndRemove(personId, (err, data) => {
     if (err) return done(err);
-    done(null /*, data*/);
+    done(null, data);
   })
 };
 
