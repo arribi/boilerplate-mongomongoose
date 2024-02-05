72c768516a2462080bcbe6410c644033856b25d8
Jesús Arribi <arribi@boxer.local>
2024-02-05 09:54:28 +0000

    Delete Many Documents with model.remove()

--- myApp.js
+++ myApp.js
@@ -105,8 +105,10 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove(nameToRemove, (err, data) => {
+    if (err) return done(err);
+    done(null, data);
+  });
 };
 
 const queryChain = (done) => {
